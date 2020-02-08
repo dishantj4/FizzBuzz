0bccf237f63931c669bc00b0273ff9f8dbdc2639
dishantj4 <dishant.jani@tcs.com>
2020-02-08 15:45:40 +0100

    Update FizzBuzz.java
    Added main code to pass unit test case and display result

--- FizzBuzz/src/main/java/com/kata/FizzBuzz.java
+++ FizzBuzz/src/main/java/com/kata/FizzBuzz.java
@@ -4,15 +4,22 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 	
-	
-		
+		for(int i = 1; i<=100; i++) {
+			System.out.println(new FizzBuzz().compute(i));
+		}
 
 	}
 
 	public String compute(int number) {
-		return null;
 
-		
+		if (number % 3 == 0 && number % 5 == 0)
+			return "FizzBuzz";
+		else if (number % 3 == 0)
+			return "Fizz";
+		else if (number % 5 == 0)
+			return "Buzz";
+		else
+			return String.valueOf(number);
 
 	}
 
